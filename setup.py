596112eb9a3123bb992b01380df5161b1ccb6db4
tek-aakash <aakash@tekkers.ai>
2024-09-26 17:08:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -82,6 +82,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Jupyter",
     ],
     python_requires=">=3.6",
